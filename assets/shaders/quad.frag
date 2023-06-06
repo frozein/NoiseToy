a04b36c8e99065afefa0434151af3db435c718d5
frozein <elwell.daniel.2@gmail.com>
2023-06-05 21:47:37 -0400

    3d noise working

--- assets/shaders/quad.frag
+++ assets/shaders/quad.frag
@@ -3,37 +3,45 @@
 in vec2 texCoord;
 out vec4 outColor;
 
-uniform sampler2D tex;
+uniform sampler2D tex2D;
+uniform sampler3D tex3D;
 
 uniform int channel;
 uniform bool grayscale;
 
+uniform bool is3D;
+uniform float slice;
+
 void main()
 {
-    vec4 texCol = texture(tex, texCoord);
-    
-    vec4 finalCol;
-    switch(channel)
-    {
-    case 0:
-        finalCol = vec4(texCol.r, 0.0, 0.0, 1.0);
-        break;
-    case 1:
-        finalCol = vec4(0.0, texCol.g, 0.0, 1.0);
-        break;
-    case 2:
-        finalCol = vec4(0.0, 0.0, texCol.b, 1.0);
-        break;
-    case 3:
-        finalCol = vec4(vec3(texCol.a), 1.0);
-        break;
-    default:
-        finalCol = texCol;
-        break;
-    }
+	vec4 texCol;
+	if(is3D)
+		texCol = texture(tex3D, vec3(texCoord, slice));
+	else
+		texCol = texture(tex2D, texCoord);
+	
+	vec4 finalCol;
+	switch(channel)
+	{
+	case 0:
+		finalCol = vec4(texCol.r, 0.0, 0.0, 1.0);
+		break;
+	case 1:
+		finalCol = vec4(0.0, texCol.g, 0.0, 1.0);
+		break;
+	case 2:
+		finalCol = vec4(0.0, 0.0, texCol.b, 1.0);
+		break;
+	case 3:
+		finalCol = vec4(vec3(texCol.a), 1.0);
+		break;
+	default:
+		finalCol = texCol;
+		break;
+	}
 
-    if(grayscale)
-        finalCol = vec4(vec3(max(max(finalCol.r, finalCol.g), finalCol.b)), 1.0);
+	if(grayscale)
+		finalCol = vec4(vec3(max(max(finalCol.r, finalCol.g), finalCol.b)), 1.0);
 
-    outColor = finalCol;
+	outColor = finalCol;
 } 
--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 //--------------------------------------------------------------------------------------------------------------------------------//
 //GLOBALS:
 
-#define WORK_GROUP_SIZE 16
+#define WORK_GROUP_SIZE 8
 
 GLuint g_windowW = 800;
 GLuint g_windowH = 800;
@@ -101,27 +101,48 @@ int main()
 	//compile shaders:
 	//---------------------------------
 
-	//fractal shader:
-	int noiseShader = compile_shader("shaders/noise.comp", GL_COMPUTE_SHADER);
-	if(noiseShader < 0)
+	//noise shader 2D:
+	int noiseShader2D = compile_shader("shaders/noise2D.comp", GL_COMPUTE_SHADER);
+	if(noiseShader2D < 0)
 	{
-		printf("ERROR - COULD NOT COMPILE NOISE SHADER");
+		printf("ERROR - COULD NOT COMPILE 2D NOISE SHADER");
 		return -1;
 	}
 
-	GLuint noiseProgram = glCreateProgram();
-	glAttachShader(noiseProgram, noiseShader);
-	glLinkProgram(noiseProgram);
+	GLuint noiseProgram2D = glCreateProgram();
+	glAttachShader(noiseProgram2D, noiseShader2D);
+	glLinkProgram(noiseProgram2D);
 
 	GLint linkSuccess;
-	glGetProgramiv(noiseProgram, GL_LINK_STATUS, &linkSuccess);
+	glGetProgramiv(noiseProgram2D, GL_LINK_STATUS, &linkSuccess);
 	if(!linkSuccess)
 	{
-		printf("ERROR - COULD NOT LINK NOISE PROGRAM\n");
+		printf("ERROR - COULD NOT LINK 2D NOISE PROGRAM\n");
 		return -1;
 	}
 
-	glDeleteShader(noiseShader);
+	glDeleteShader(noiseShader2D);
+
+	//noise shader 3D:
+	int noiseShader3D = compile_shader("shaders/noise3D.comp", GL_COMPUTE_SHADER);
+	if(noiseShader3D < 0)
+	{
+		printf("ERROR - COULD NOT COMPILE 3D NOISE SHADER");
+		return -1;
+	}
+
+	GLuint noiseProgram3D = glCreateProgram();
+	glAttachShader(noiseProgram3D, noiseShader3D);
+	glLinkProgram(noiseProgram3D);
+
+	glGetProgramiv(noiseProgram3D, GL_LINK_STATUS, &linkSuccess);
+	if(!linkSuccess)
+	{
+		printf("ERROR - COULD NOT LINK 3D NOISE PROGRAM\n");
+		return -1;
+	}
+
+	glDeleteShader(noiseShader3D);
 
 	//quad shader:
 	int vertShader = compile_shader("shaders/quad.vert", GL_VERTEX_SHADER);
@@ -191,20 +212,29 @@ int main()
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(long long)(3 * sizeof(GL_FLOAT)));
 	glEnableVertexAttribArray(1);
 
-	//generate texture:
+	//generate textures:
 	//---------------------------------
 	GLuint textureSize = 128;
-
 	GLuint lastTextureSize = textureSize; //for determining if texture needs to be resized
 
-	GLuint noiseTexture;
-	glGenTextures(1, &noiseTexture);
-	glBindTexture(GL_TEXTURE_2D, noiseTexture);
+	GLuint noiseTexture2D;
+	glGenTextures(1, &noiseTexture2D);
+	glBindTexture(GL_TEXTURE_2D, noiseTexture2D);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, textureSize, textureSize, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+
+	GLuint noiseTexture3D;
+	glGenTextures(1, &noiseTexture3D);
+	glBindTexture(GL_TEXTURE_3D, noiseTexture3D);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexImage3D(GL_TEXTURE_3D, 0, GL_RGBA32F, textureSize, textureSize, textureSize, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 
 	//generate data buffer:
 	//---------------------------------
@@ -214,13 +244,16 @@ int main()
 	glBufferData(GL_UNIFORM_BUFFER, sizeof(NoiseData) * 8, NULL, GL_DYNAMIC_DRAW);
 
 	//parameter declarations:
-	//---------------------------------	
-	const char* textureSizeNames[8] = {"16", "32", "64", "128", "256", "512", "1024", "2048"};
+	//---------------------------------
+	bool is3D = false;
+
+	const char* textureSizeNames[7] = {"16", "32", "64", "128", "256", "512", "1024"};
 	int selectedTextureSize = 3;
 
 	const char* channelNames[5] = {"R", "G", "B", "A", "ALL"};
 	GLint channelView = 0;
 	bool grayscaleChannel = true;
+	float slice = 0.0f;
 
 	const char* layeringNames[3] = {"No Layering", "Multiply", "Average"};
 	GLuint layeringType = 0;
@@ -255,14 +288,27 @@ int main()
 		glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(NoiseData) * 8, noiseData);
 
 		//dispatch compute shader:
-		glUseProgram(noiseProgram);
-
-		glUniform1ui(glGetUniformLocation(noiseProgram, "layeringType"), layeringType);
-		glBindImageTexture(0, noiseTexture, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
-		glBindBufferBase(GL_UNIFORM_BUFFER, 0, noiseDataBuffer);
-
 		GLuint numWorkGroups = (GLuint)ceilf((float)textureSize / WORK_GROUP_SIZE);
-		glDispatchCompute(numWorkGroups, numWorkGroups, 1);
+		if(is3D)
+		{
+			glUseProgram(noiseProgram3D);
+
+			glUniform1ui(glGetUniformLocation(noiseProgram3D, "layeringType"), layeringType);
+			glBindImageTexture(0, noiseTexture3D, 0, GL_TRUE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+			glBindBufferBase(GL_UNIFORM_BUFFER, 0, noiseDataBuffer);
+
+			glDispatchCompute(numWorkGroups, numWorkGroups, numWorkGroups);
+		}
+		else
+		{
+			glUseProgram(noiseProgram2D);
+
+			glUniform1ui(glGetUniformLocation(noiseProgram2D, "layeringType"), layeringType);
+			glBindImageTexture(0, noiseTexture2D, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+			glBindBufferBase(GL_UNIFORM_BUFFER, 0, noiseDataBuffer);
+
+			glDispatchCompute(numWorkGroups, numWorkGroups, 1);
+		}
 		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
 
 		//run final quad shader:
@@ -277,8 +323,16 @@ int main()
 		glUniform2fv(glGetUniformLocation(quadProgram, "scale"), 1, scale);
 		glUniform1i(glGetUniformLocation(quadProgram, "channel"), channelView);
 		glUniform1ui(glGetUniformLocation(quadProgram, "grayscale"), (GLboolean)grayscaleChannel);
+		glUniform1ui(glGetUniformLocation(quadProgram, "is3D"), (GLuint)is3D);
+		glUniform1f(glGetUniformLocation(quadProgram, "slice"), slice);
+
+		glUniform1i(glGetUniformLocation(quadProgram, "tex2D"), 0);
+		glUniform1i(glGetUniformLocation(quadProgram, "tex3D"), 1);
+
 		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, noiseTexture);
+		glBindTexture(GL_TEXTURE_2D, noiseTexture2D);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_3D, noiseTexture3D);
 
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(0 * sizeof(unsigned int)));
 
@@ -289,13 +343,18 @@ int main()
 
 		ImGui::Begin("Texture Settings");
 
-		ImGui::Combo("Size", &selectedTextureSize, textureSizeNames, 8);
+		ImGui::Combo("Size", &selectedTextureSize, textureSizeNames, 7);
 		textureSize = strtoul(textureSizeNames[selectedTextureSize], NULL, 10);
+		ImGui::SameLine();
+		ImGui::Checkbox("3D", &is3D);
 
 		ImGui::SetNextItemWidth(ImGui::GetWindowSize().x * 0.25f);
 		ImGui::Combo("Channel", &channelView, channelNames, 5);
 		ImGui::SameLine();
 		ImGui::Checkbox("Grayscale", &grayscaleChannel);
+
+		if(is3D)
+			ImGui::SliderFloat("Slice", &slice, 0.0f, 1.0f);
 
 		ImGui::Combo("Layering Type", (int*)&layeringType, layeringNames, 3);
 
@@ -314,8 +373,11 @@ int main()
 		//resize texture if needed:
 		if(textureSize != lastTextureSize)
 		{
-			glBindTexture(GL_TEXTURE_2D, noiseTexture);
+			glBindTexture(GL_TEXTURE_2D, noiseTexture2D);
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, textureSize, textureSize, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+
+			glBindTexture(GL_TEXTURE_3D, noiseTexture3D);
+			glTexImage3D(GL_TEXTURE_3D, 0, GL_RGBA32F, textureSize, textureSize, textureSize, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 
 			lastTextureSize = textureSize;
 		}
@@ -326,10 +388,15 @@ int main()
 	}
 
 	//clean up and return:
-	glDeleteTextures(1, &noiseTexture);
+	glDeleteTextures(1, &noiseTexture2D);
+	glDeleteTextures(1, &noiseTexture3D);
+
 	glDeleteVertexArrays(1, &quadVAO);
+	
 	glDeleteProgram(quadProgram);
-	glDeleteProgram(noiseProgram);
+	glDeleteProgram(noiseProgram2D);
+	glDeleteProgram(noiseProgram3D);
+	
 	glfwTerminate();
 
 	return 0;
@@ -347,7 +414,7 @@ void render_noise_ui(NoiseData* data, bool primary)
 	ImGui::PushItemWidth(ImGui::GetWindowSize().x * 0.5f);
 
 	const char* noiseNames[3] = {"Perlin", "Worly", "Worly (Inverted)"};
-	ImGui::Combo("Noise Type", (int*)&data->type, noiseNames, 3);	
+	ImGui::Combo("Noise Type", (int*)&data->type, noiseNames, 3);
 	
 	ImGui::InputFloat("Seed", &data->seed, 1.0f, 10.0f);
 
